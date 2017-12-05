4e16005a5a3575403ea279730f46dae509a641ca
dave.kavanagh <dave.kavanagh@workday.com>
2017-12-05 13:40:05 +0000

    changed socket url

--- views/js/sticky.js
+++ views/js/sticky.js
@@ -16,7 +16,7 @@ $(document).ready(function() {
 	makeDraggable();
 
 	//socket.io connection for 'chat' functionality
-	var socket = io("http://localhost:3000");
+	var socket = io("https://sticky-board-app.herokuapp.com:5000/");
 	socket.emit("join", sessionID);
 
 	//display sticky info received from server
